b9439c82b2c44db2f97e9935736056e6908ea8f8
loksland <github@maker.com.au>
2021-11-18 08:56:39 +1000

    Improved performance, sped up transition, reduced blur amount

--- src/trans/over.js
+++ src/trans/over.js
@@ -2,6 +2,8 @@ import { nav,scaler } from './../storymode.js';
 
 export const id = 'over';
 
+const BLUR_FG = false;
+const APPLY_BG_ALPHA = false;
 export default class OverTrans {
   
   constructor(scene, scenePrev = null, isModal = false, transConfigStr = null){
@@ -11,27 +13,30 @@ export default class OverTrans {
     this.isModal = isModal;
     this.isTransparent = this.isModal;
     
-    this.maxBlur = 1;
+    this.maxBlur = 5.0
     
   }
   
   performIn(onInCallback){
     
-    const dur = 0.6;
+    const dur = 0.4;
     
     this.scene.visible = true;
     
     if (this.scenePrev){
-    
-      const filterOut = new PIXI.filters.AlphaFilter(1.0);
-      this.scenePrev.filters = [filterOut];
-      gsap.to(filterOut, dur, {pixi:{alpha: this.isModal ? 0.5 : 0.0}, ease: Linear.easeNone});      
-      gsap.to(this.scenePrev, dur, {pixi: {blur:14}, ease:Power3.easeIn});
+      if (APPLY_BG_ALPHA){
+        const filterOut = new PIXI.filters.AlphaFilter(1.0);
+        this.scenePrev.filters = [filterOut];
+        gsap.to(filterOut, dur, {pixi:{alpha: this.isModal ? 0.5 : 0.0}, ease: Linear.easeNone});      
+      }
+      gsap.to(this.scenePrev, dur, {pixi: {blur:this.maxBlur}, ease:Power3.easeIn});
       
     }
 
     if (!this.isModal && !nav.isScenePresentedWithTransparentBg()){
+      
       gsap.to(nav.bg, dur, {pixi: {tint:this.scene.bgColor}, ease:Linear.easeNone});
+      
     }
     
     const filterIn = new PIXI.filters.AlphaFilter(0.0);
@@ -40,7 +45,12 @@ export default class OverTrans {
     
     this.scene.position.set(scaler.stageW*0.5, scaler.stageH*0.5)
     this.scene.pivot.set(scaler.stageW*0.5, scaler.stageH*0.5)
-    gsap.from(this.scene, dur, {pixi: {scale: 1.3, blur:this.maxBlur} , ease:Power3.easeOut, onComplete:this.onIn.bind(this), onCompleteParams: [onInCallback]});
+    
+    let tw = {scale: 1.3};
+    if (BLUR_FG){
+      tw.blur = this.maxBlur;
+    }
+    gsap.from(this.scene, dur, {pixi: tw, ease:Power3.easeOut, onComplete:this.onIn.bind(this), onCompleteParams: [onInCallback]});
     
   }
   
@@ -61,7 +71,7 @@ export default class OverTrans {
   
   performOut(onOutCallback){
     
-    let dur = 0.4;
+    let dur = 0.3;
     
     this.scene.position.set(scaler.stageW*0.5, scaler.stageH*0.5)
     this.scene.pivot.set(scaler.stageW*0.5, scaler.stageH*0.5)
@@ -69,14 +79,19 @@ export default class OverTrans {
     const filterOut = new PIXI.filters.AlphaFilter(1.0);
     this.scene.filters = [filterOut];
     gsap.to(filterOut, dur, {pixi:{alpha:0.0}, ease:Linear.easeNone});
+    
+    if (BLUR_FG){
+      gsap.to(this.scene, dur, {pixi: {blur:this.maxBlur}, ease:Power3.easeOut});
+    }
     gsap.to(this.scene, dur, {pixi: {scale: 0.9}, ease:Sine.easeIn});
-    gsap.to(this.scene, dur, {pixi: {blur:this.maxBlur}, ease:Power3.easeOut});
+    
       
     const delay = 0.0;
-    dur = 0.8;
-    
-    gsap.to(this.scenePrev.filters[0], dur, {pixi:{alpha:1.0}, ease:Linear.easeNone, delay:delay});
-    gsap.to(this.scenePrev, dur, {pixi: {blur:0}, ease:Power3.easeIn, delay:delay, onComplete:this.onOut.bind(this), onCompleteParams: [onOutCallback]});
+    dur = 0.5;
+    if (APPLY_BG_ALPHA){
+      gsap.to(this.scenePrev.filters[0], dur, {pixi:{alpha:1.0}, ease:Linear.easeNone, delay:delay});
+    }
+    gsap.to(this.scenePrev, dur, {pixi: {blur:0.0}, ease:Power3.easeIn, delay:delay, onComplete:this.onOut.bind(this), onCompleteParams: [onOutCallback]});
     
   }
   
